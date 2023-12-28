59728e02a08ace2d9768943f9b76d137f18dacc4
FabianoSpuriFachim <FabianoSpuriFachim@MAQUINA03>
2023-12-28 12:11:17 -0300

    Ajuste Tabela Cotação

--- 12- Cotacao.sql
+++ 12- Cotacao.sql
@@ -1,10 +1,10 @@
 
-
+/****** Object:  Table [dbo].[cotacao]    Script Date: 12/28/2023 12:10:36 PM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[cotacao]') AND type in (N'U'))
 DROP TABLE [dbo].[cotacao]
 GO
 
-
+/****** Object:  Table [dbo].[cotacao]    Script Date: 12/28/2023 12:10:36 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -19,59 +19,47 @@ CREATE TABLE [dbo].[cotacao](
 	[CotacaoStatus_Id] [int] NOT NULL,
 	[Vendedor] [varchar](30) NULL,
 	[DataPostagem] [datetime] NULL,
-	[CondicoesPagamento_Id] [int]  NULL,
-	[Frete_Id] [int]  NULL,
+	[CondicoesPagamento_Id] [int] NULL,
+	[Frete_Id] [int] NULL,
 	[OutrasDespesas] [decimal](19, 2) NULL,
-    [ValorFrete] [decimal](19, 2) NULL,
+	[ValorFrete] [decimal](19, 2) NULL,
 	[ValorFreteForaNota] [decimal](19, 2) NULL,
 	[ValorSeguro] [decimal](19, 2) NULL,
 	[ValorDesconto] [decimal](19, 2) NULL,
 	[Observacao] [varchar](255) NULL,
 	[PrazoMaximoCotacao] [datetime] NULL,
 	[DataEntregaDesejavel] [datetime] NULL,
-    [NomeUsuarioCadastro] [varchar](50) NOT NULL,
+	[NomeUsuarioCadastro] [varchar](50) NOT NULL,
 	[DataCadastro] [datetime] NOT NULL,
 	[NomeUsuarioAlteracao] [varchar](50) NULL,
 	[DataAlteracao] [datetime] NULL,
-	[Guid] [uniqueidentifier] NOT NULL,	
+	[Guid] [uniqueidentifier] NOT NULL,
  CONSTRAINT [PK_cotacao] PRIMARY KEY CLUSTERED 
 (
-	[id] ASC
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-
-ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_fornecedor] FOREIGN KEY(Fornecedor_Id)
-REFERENCES [dbo].[fornecedor] ([id])
-GO
-
-ALTER TABLE [dbo].[cotacao] CHECK CONSTRAINT [FK_tb_cotacao_fornecedor]
-GO
-
-ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_motivo] FOREIGN KEY(Motivo_Id)
-REFERENCES [dbo].[cotacao_motivo] ([Id])
-GO
-
-ALTER TABLE [dbo].[cotacao] CHECK CONSTRAINT [FK_tb_cotacao_motivo]
-GO
-
-ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_cotacaostatus] FOREIGN KEY(CotacaoStatus_Id)
+ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_cotacaostatus] FOREIGN KEY([CotacaoStatus_Id])
 REFERENCES [dbo].[cotacao_status] ([Id])
 GO
 
 ALTER TABLE [dbo].[cotacao] CHECK CONSTRAINT [FK_tb_cotacao_cotacaostatus]
 GO
 
-CREATE NONCLUSTERED INDEX [IX_Cotacao] ON [dbo].[cotacao]
-(
-	[Fornecedor_Id] ASC,
-	[IdCotacao] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_fornecedor] FOREIGN KEY([Fornecedor_Id])
+REFERENCES [dbo].[fornecedor] ([Id])
 GO
 
-CREATE NONCLUSTERED INDEX [IX_CotacaoGuid] ON [dbo].[cotacao]
-(
-	[Guid] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+ALTER TABLE [dbo].[cotacao] CHECK CONSTRAINT [FK_tb_cotacao_fornecedor]
 GO
+
+ALTER TABLE [dbo].[cotacao]  WITH CHECK ADD  CONSTRAINT [FK_tb_cotacao_motivo] FOREIGN KEY([Motivo_Id])
+REFERENCES [dbo].[cotacao_motivo] ([Id])
+GO
+
+ALTER TABLE [dbo].[cotacao] CHECK CONSTRAINT [FK_tb_cotacao_motivo]
+GO
+
+
